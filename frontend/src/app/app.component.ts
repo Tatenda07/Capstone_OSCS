43fedaa41e9a3c88bc596cfaf428e77ba05b0379
rutadeline <uwibambe_adeline@yahoo.com>
2022-03-29 19:46:52 +0800

    update warning budget

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { StudentService } from './shared/services/student.service';
 import { UserService } from './shared/services/user.service';
 import { Router } from '@angular/router';
+import { Platform } from '@angular/cdk/platform';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,79 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'frontend';
 
+  isOnline: boolean;
+  modalVersion: boolean;
+  modalPwaEvent: any;
+  modalPwaPlatform: string|undefined;
+
   //show or hide side menu
   displayMenu = true;
 
-  constructor(public studentService: StudentService, public userService: UserService, private router: Router) { }
-
+  constructor(public studentService: StudentService, public userService: UserService, private router: Router, private platform: Platform,
+    private swUpdate: SwUpdate) { 
+    
+    this.isOnline = false;
+    this.modalVersion = false;
+    }
+    public ngOnInit(): void {
+      this.updateOnlineStatus();
+  
+      window.addEventListener('online',  this.updateOnlineStatus.bind(this));
+      window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  
+      if (this.swUpdate.isEnabled) {
+        this.swUpdate.versionUpdates.pipe(
+          filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+          map((evt: any) => {
+            console.info(`currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]`);
+            this.modalVersion = true;
+          }),
+        );
+      }
+  
+      this.loadModalPwa();
+    }
+  
+    private updateOnlineStatus(): void {
+      this.isOnline = window.navigator.onLine;
+      console.info(`isOnline=[${this.isOnline}]`);
+    }
+  
+    public updateVersion(): void {
+      this.modalVersion = false;
+      window.location.reload();
+    }
+  
+    public closeVersion(): void {
+      this.modalVersion = false;
+    }
+  
+    private loadModalPwa(): void {
+      if (this.platform.ANDROID) {
+        window.addEventListener('beforeinstallprompt', (event: any) => {
+          event.preventDefault();
+          this.modalPwaEvent = event;
+          this.modalPwaPlatform = 'ANDROID';
+        });
+      }
+  
+      if (this.platform.IOS && this.platform.SAFARI) {
+        const isInStandaloneMode = ('standalone' in window.navigator) && ((<any>window.navigator)['standalone']);
+        if (!isInStandaloneMode) {
+          this.modalPwaPlatform = 'IOS';
+        }
+      }
+    }
+  
+    public addToHomeScreen(): void {
+      this.modalPwaEvent.prompt();
+      this.modalPwaPlatform = undefined;
+    }
+  
+    public closePwa(): void {
+      this.modalPwaPlatform = undefined;
+    }
+  
   toogleMenu() {
     this.displayMenu = !this.displayMenu;
   }
