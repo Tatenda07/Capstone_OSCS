f57490c02241ed33b297417e27f88d09dab0c1e6
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2022-05-09 10:27:44 +0200

    Update nlp.js

--- backend/controllers/nlp.js
+++ backend/controllers/nlp.js
@@ -10,19 +10,29 @@ spellCorrector.loadDictionary();
 // sentiment alalyzer 
 exports.sentimentAnalyzer = (req, res, next) => {
   const { review } = req.body;
+  // convert contractions into standard lexicon (e.g I'm -> I am)
   const lexedReview = aposToLexForm(review);
+  // convert text data to lowercase
   const casedReview = lexedReview.toLowerCase();
+  // remove non-alphobetical and special characters
   const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
 
+  // Tokenization - splitting a text into its individual meaningful units
   const { WordTokenizer } = natural;
   const tokenizer = new WordTokenizer();
   const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
 
+  // correct misspelled words
   tokenizedReview.forEach((word, index) => {
     tokenizedReview[index] = spellCorrector.correct(word);
   })
+  // remove stop words
   const filteredReview = SW.removeStopwords(tokenizedReview);
 
+  /*
+    Stemming - word normalization in NLP used to convert derived or inflected words to their base or root form
+    e.g The stemmer algorithm is expected to reduce the words “giving,” “gave,” and “giver” to their root word, “give.”
+  */
   const { SentimentAnalyzer, PorterStemmer } = natural;
   const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
   const analysis = analyzer.getSentiment(filteredReview);
